16baff3d6e8c743a837e9fda731e236b7020e459
BiznisMarketing <125213700+BiznisMarketing@users.noreply.github.com>
2023-02-13 23:00:49 +0100

    Update job_application.py

--- job_application.py
+++ job_application.py
@@ -9,21 +9,21 @@ if answer1_statement == "Yes" or answer1_statement == "yes" or answer1_statement
 elif answer1_statement == "No" or answer1_statement == "no" or answer1_statement == "NO":
     answer1 = False
 else:
-    print("Ivalid value.")
+    print("Invalid value.")
 
 if answer2_statement == "Yes" or answer2_statement == "yes" or answer2_statement == "YES":
     answer2 = True
 elif answer2_statement == "No" or answer2_statement == "no" or answer2_statement == "NO":
     answer2 = False
 else:
-    print("Ivalid value.")
+    print("Invalid value.")
 
 if answer3_statement == "Yes" or answer3_statement == "yes" or answer3_statement == "YES":
     answer3 = True
 elif answer3_statement == "No" or answer3_statement == "no" or answer3_statement == "NO":
     answer3 = False
 else:
-    print("Ivalid value.")
+    print("Invalid value.")
 
 if answer1 and answer2 and answer3:
     print("Nika should be hired.")
